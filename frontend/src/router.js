bd34a40b146a35f2cdd2c04708f53bc9b0345e86
Caitlin Malan <cshanmalan28@gmail.com>
2025-08-30 22:57:45 +0200

    Modified Account Setup and Intergartion Successful

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,16 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import store from './store/store'; // import your Vuex store
-import { IS_USER_AUTHENTICATE_GETTER } from './store/storeconstants';
 
 const Home = () => import('./pages/welcomepage.vue');
-const Login = () => import('./pages/AccountSetUpPage.vue');
+const AccountSetUp = () => import('./pages/AccountSetUpPage.vue');
 const Product = () => import('./pages/productPage.vue');
 const AuctionSession = () => import('./pages/auctionSession.vue');
 
 const routes = [
     { path: '/', redirect: '/welcome'},
     { path: '/welcome', component: Home },
-    { path: '/account-setup', component: Login, meta: { auth: false } },
+    { path: '/account-setup', component: AccountSetUp, meta: { auth: false } },
     { path: '/product', component: Product, meta: { auth: false } },
     { path: '/auction', component: AuctionSession, meta: { auth: true } },
 ];
@@ -18,21 +16,6 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
-});
-
-// Navigation guard for auth
-router.beforeEach((to, from, next) => {
-    const isAuthenticated = store.getters[`auth/${IS_USER_AUTHENTICATE_GETTER}`];
-
-    if (to.meta.auth && !isAuthenticated) {
-        // Route requires auth but user isn't authenticated
-        next('/login');
-    } else if (!to.meta.auth && isAuthenticated && to.path === '/login') {
-        // Authenticated user trying to access login/signup
-        next('/posts');
-    } else {
-        next();
-    }
 });
 
 export default router;
