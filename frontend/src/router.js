685fbebfb3f3906afa4e6d8d11aa0f0190f94578
LiamMatthews27 <matthewsliam27@gmail.com>
2025-08-31 20:48:17 +0200

    individual and auctionhistory design

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,6 +5,8 @@ const AccountSetUp = () => import('./pages/AccountSetUpPage.vue');
 const Product = () => import('./pages/productPage.vue');
 const AuctionSession = () => import('./pages/auctionSession.vue');
 const CarDetails = () => import('./pages/CarDetailsPage.vue');
+const AuctionHistory = () => import('./pages/AuctionHistory.vue');
+const IndividualForm = () => import('./pages/IndividualForm.vue');
 
 const routes = [
     { path: '/', redirect: '/welcome' },
@@ -13,6 +15,8 @@ const routes = [
     { path: '/product', component: Product, meta: { auth: false } },
     { path: '/auction', component: AuctionSession, meta: { auth: true } },
     { path: '/cardetails/:vin', component: CarDetails, meta: { auth: false } },
+    { path: '/auction-history', component: AuctionHistory, meta: { auth: true } },
+    { path: '/individual-form', component: IndividualForm, meta: { auth: false } },
 ];
 
 const router = createRouter({
